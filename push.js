4c7de0aa50fe0d1e3f73a9c38454b8164280eac9
Konrad Soares <konrad.soares@gmail.com>
2020-05-23 02:34:25 +0100

    fix3

--- push.js
+++ push.js
@@ -8,6 +8,15 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:konrad.soares@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+"endpoint":"https://fcm.googleapis.com/fcm/send/eRoeqJuN3uA:APA91bEZ9pVqEeFGOO390tHJWSpIMRz6iECqOT4H_0iaa3FGK_nuPdjEyussTCoUph62nTVQybuVR77vGw6cjOLqYhT2iBUUKZh_L02FxC6iN48l9cSjWrcbMuJ_RyBXK4Mzr71zRk2T",
+"expirationTime":null,
+"keys":
+    {
+    "p256dh":"BKhz8igMn4-MEkaSDiwbvRpNhpEM8BRHPOSjPB39u4PkLxKzal8eGjZ0HnRBwpn539F8tGoFaXo-RQiYhzpXVzc",
+    "auth":"Udgvd5LsyWBhi_k47MnCEw"
+    }
+}
+
 
 push.sendNotification(sub, 'text message')  
