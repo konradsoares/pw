246e8926cf1438e628cb77c105d927d89554974f
Konrad Soares <konrad.soares@gmail.com>
2020-05-23 02:10:58 +0100

    fix

--- push.js
+++ push.js
@@ -1,5 +1,13 @@
 var push = require('web-push');
 
 
-let vapidKeys = push.generateVAPIDKeys();
-console.log(vapidKeys);
+let vapidKeys = {
+    publicKey: 'BPZ_BevDCkLTgpTjDu967m0pAKHqL6qOKbF4k8bMIICV-oVwIv9SP-kq5rqhUxvXo0Y0GPdafRFvZosuK36IIpU',
+    privateKey: 'T3R8C3BYgx_Y2Ftk6pqGThnDHClFmVlGlEz_XRs8ImU'
+  }
+
+push.setVapidDetails('mailto:konrad.soares@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
+
+let sub = {};
+
+push.sendNotification(sub, 'text message')  
